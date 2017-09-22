4a6f1f523a816bde872d9dd2e81babdde502105b
Cheney Shreve <cheney.shreve@gmail.com>
2017-09-22 23:25:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,17 +23,14 @@ function sayHiToGrandma(string) {
   var lowercase = 'hello'
   lowercase.toLowerCase() === lowercase // true
   return ("I can't hear you")
-  
+
   var uppercase = 'HELLO'
   uppercase.toUpperCase() === uppercase // true
   return("YES INDEED!")
 
   var mixedCase = 'I love you, Grandma.'
   mixedCase.toLowerCase() === mixedCase // false
-
   mixedCase.toUpperCase() === mixedCase // false
-
-  
+  return("I love you, too.")
 
 }
-
