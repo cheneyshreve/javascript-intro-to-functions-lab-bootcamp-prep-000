949a5b7a17bbcad27a3a7bb12732f9f8cacaf028
Cheney Shreve <cheney.shreve@gmail.com>
2017-09-22 22:54:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  var uppercase = 'HELLO'
+  uppercase.toUpperCase() === uppercase // true
+  console.log(uppercase)
+}
+
+function logWhisper(string) {
+  var lowercase = 'hello'
+  lowercase.toLowerCase() === lowercase // true
+  console.log(lowercase)
+}
+
+function sayHiToGrandma(string) {
+
+  logWhisper(string)
+  console.log("I can't hear you!")
+
+  logShout(string)
+  console.log("YES INDEED!")
+
+  var mixedCase = 'I love you, Grandma.'
+  mixedCase.toLowerCase() === mixedCase // false
+  mixedCase.toUpperCase() === mixedCase // false
+
+  console.log("I love you, too!")
+
+}
